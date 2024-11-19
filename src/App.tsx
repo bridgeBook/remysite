f7acb2a0f42e39e731c77bea1dc63aaf6a1a4d9e
bridgeBook <johndoe@example.cok9mm10feet@yahoo.co.jp>
2024-11-19 23:03:07 +0900

    demo2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="top">aaa</div>
+      <div className="top">
+        <div>◯</div>
+        <div className="topMenu">
+          <div className="topMenu_about">about</div>
+          <div className="topMenu_sample">sample</div>
+          <div className="topMenu_contact">contact</div>
+        </div>
+      </div>
       <div className="mid">aaa</div>
       <div className="bot">aaa</div>
     </>
