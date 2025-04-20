6e4e284f32afae8e7a681913c7d6b68dbc040241
bridgeBook <k9mm10feet@yahoo.co.jp>
2025-04-20 23:36:56 +0900

    ホスティング設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ↓リポジトリ名に合わせて書き換える（例：remysite）
+const repoName = 'remysite'
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
